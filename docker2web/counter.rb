88bfa458d4b760dcc8c1f770bbb2b35783f30a37
ubuntu <webstd.design@gmail.com>
2017-11-05 13:21:53 -0800

    test1

---
+++ docker2web/counter.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'redis'
+
+set :bind, '0.0.0.0'
+
+configure do
+	$redis = Redis.new(:host=>'redis')
+end
+
+get '/' do
+	count = $redis.incr('count')
+
+	"<h1>Hello</h1>"\
+	"<p>Page has been viewed #{count} times</p>"
+end
